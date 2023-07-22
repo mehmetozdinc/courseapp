2d6f05833fa603853c1d0d01e39d6ddc28ba8d86
Mehmet Ozdinc <mehmetozdincc@gmail.com>
2023-07-22 14:45:36 +0300

    10. bölüm de takip edilerek yayınlama öncesi son değişiklikler yapıldı. 10. bölüm yayınlama harici tamamlandı

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('kurslar/', include('courses.urls')),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
